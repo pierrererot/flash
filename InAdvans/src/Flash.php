bfd7724c7900d4aeb76ff0ae27f633cf36f6b052
prerot <pierre.rerot@inadvans.com>
2019-01-24 15:11:49 +0100

    init project

---
+++ InAdvans/src/Flash.php
@@ -0,0 +1,64 @@
+<?php
+
+namespace Flash;
+use Yii;
+
+/**
+ * Class Flash
+ */
+class Flash
+{
+    /**
+     * @param string $message
+     * @param bool   $removeAfterAccess
+     */
+    public static function error($message, $removeAfterAccess = true)
+    {
+        Yii::$app->session->addFlash('danger', $message, $removeAfterAccess);
+    }
+
+    /**
+     * @param string $message
+     * @param bool   $removeAfterAccess
+     */
+    public static function alert($message, $removeAfterAccess = true)
+    {
+        Yii::$app->session->addFlash('danger', $message, $removeAfterAccess);
+    }
+
+    /**
+     * @param string $message
+     * @param bool   $removeAfterAccess
+     */
+    public static function danger($message, $removeAfterAccess = true)
+    {
+        Yii::$app->session->addFlash('danger', $message, $removeAfterAccess);
+    }
+
+    /**
+     * @param string $message
+     * @param bool   $removeAfterAccess
+     */
+    public static function warning($message, $removeAfterAccess = true)
+    {
+        Yii::$app->session->addFlash('warning', $message, $removeAfterAccess);
+    }
+
+    /**
+     * @param string $message
+     * @param bool   $removeAfterAccess
+     */
+    public static function success($message, $removeAfterAccess = true)
+    {
+        Yii::$app->session->addFlash('success', $message, $removeAfterAccess);
+    }
+
+    /**
+     * @param string $message
+     * @param bool   $removeAfterAccess
+     */
+    public static function info($message, $removeAfterAccess = true)
+    {
+        Yii::$app->session->addFlash('info', $message, $removeAfterAccess);
+    }
+}
